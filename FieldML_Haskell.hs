ceb0bb92f1f2e5100a8cfd527c72c9dfb94e3112
Randall Britten <r.britten@auckland.ac.nz>
2012-05-17 16:17:28 +1200

    Added type for Maps (I prefer to call them this rather than Fields). The whole thing is still a bit vague.
    Could add validation checks, e.g. that composition of Maps is domain/codomain compatible.

--- FieldML_Haskell.hs
+++ FieldML_Haskell.hs
@@ -1,22 +1,48 @@
 import qualified Data.Set as Set
+import qualified Data.Map as Map
 import Text.Show.Functions
 
 type Label = String
 type SetOfLabels = Set.Set Label
 
-data Manifold = 
-  Reals |
-  Labels SetOfLabels | 
-  Product [Manifold] |
-  DisjointUnion SetOfLabels (Label->Manifold)
+labelsFromIntegerRange :: Int->Int->SetOfLabels
+labelsFromIntegerRange a b =
+  Set.fromList $ map show [a..b]
+
+data RealExpression =
+    Plus RealExpression RealExpression
+  | Minus RealExpression RealExpression
+  | Times RealExpression RealExpression
+  | Divide RealExpression RealExpression
+  -- Etc
+
+data Map = 
+    Map { domain :: Manifold, codomain :: Manifold }
+  | RealExpression
+  | Compose Map Map
+  | Project { factor :: Int, domain :: Manifold, codomain :: Manifold }
+  | BooleanMap { domain :: Manifold }
   deriving (Show)
+
+data Manifold = 
+    Reals
+  | Labels SetOfLabels
+  | Product [Manifold]
+  | DisjointUnion SetOfLabels (Label->Manifold) 
+  | SimpleSubset Map
+  deriving (Show)
+
+data FieldmlModel = FieldmlModel {
+  namedManifolds :: Map.Map String Manifold,
+  namedFields    :: Map.Map String Map
+}
 
 -- Tests
 real2 = Product [Reals, Reals]
 real3 = Product [Reals, Reals, Reals]
 
-elementIds = Set.fromList ([ "1","3","2","4" ])
-
+elementIds = labelsFromIntegerRange 1 4
+  
 f :: Label->Manifold
 f "1" = Reals
 f _ = Reals
@@ -24,3 +50,7 @@ f _ = Reals
 m1 = DisjointUnion elementIds f
 
 m2 = Product [real2, Labels elementIds]
+
+map1 = Project { factor=1, domain=m2, codomain=real2}
+
+-- Just playing with Haskell Syntax here for convenience.  Will eventually delete everything below this line, and this comment.
