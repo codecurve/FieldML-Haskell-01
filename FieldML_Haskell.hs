2c8d808d8ab8b6f2cad9f04adc71f0ea44dcd205
Randall Britten <r.britten@auckland.ac.nz>
2012-05-18 15:24:51 +1200

    Added "RealVariable" for real expressions of that type. Renamed "Manifold" to the more general "TopologicalSpace" Added unit line segment example.

--- FieldML_Haskell.hs
+++ FieldML_Haskell.hs
@@ -4,9 +4,13 @@ import Text.Show.Functions
 
 -- By Randall Britten
 -- Auckland Bioengineering Institute
+-- 2012
 -- University of Auckland
+-- Permission granted to redistribute in source code or binary form.  No warranty of any kind is given.
 --
--- The ideas here were strongly influenced by Andrew Miller's "ModML".
+-- The ideas here were strongly influenced by Andrew Miller's "ModML", with some code copied directly from ModML
+
+-- This is under construction
 
 type Label = String
 type SetOfLabels = Set.Set Label
@@ -15,23 +19,29 @@ labelsFromIntegerRange :: Int->Int->SetOfLabels
 labelsFromIntegerRange a b =
   Set.fromList $ map show [a..b]
 
-data BoolExpression =
+data BooleanExpression =
   -- A constant true or false.
-  BoolConstant Bool |
+  BooleanConstant Bool |
   -- Logical and of two expressions.
-  And BoolExpression BoolExpression |
+  And BooleanExpression BooleanExpression |
   -- Logical not of an expression.
-  Not BoolExpression |
+  Not BooleanExpression |
   -- Logical or of two expressions.
-  Or BoolExpression BoolExpression |
+  Or BooleanExpression BooleanExpression |
   LessThan RealExpression RealExpression |
   Equal RealExpression RealExpression
   deriving (Show)
   
+data RealVariable = RealVariable String  
+  deriving (Show)
+  
 data RealExpression =
+  -- Any real value, as a constant.
   RealConstant Double |
+  -- A free variable...
+  RealVariableE RealVariable |  
   -- If x {- then -} b {- else -} b
-  If BoolExpression RealExpression RealExpression |
+  If BooleanExpression RealExpression RealExpression |
   Plus RealExpression RealExpression |
   Minus RealExpression RealExpression |
   Times RealExpression RealExpression |
@@ -40,32 +50,34 @@ data RealExpression =
   -- Etc
 
 data Map = 
-  Map { domain :: Manifold, codomain :: Manifold } |
+  Map { domain :: TopologicalSpace, codomain :: TopologicalSpace } |
   -- The domain and codomain are both Reals
   RealExpression |
   Compose Map Map |
-  Project { factor :: Int, domain :: Manifold, codomain :: Manifold } |
-  -- The codomain is implicitly BooleanManifold
-  BooleanMap { domain :: Manifold } |
-  -- The domain must be the CartesianProduct of n discrete manifolds, with a total cardinality equal to the number of parameters, 
-  -- and n equal to the dimensionality of the parameter source.
-  FromParameterSource { domain :: Manifold }
+  Project { factor :: Int, domain :: TopologicalSpace, codomain :: TopologicalSpace } |
+  -- The codomain is implicitly BooleanTopologicalSpace
+  BooleanMap { domain :: TopologicalSpace, predicate :: BooleanExpression } |
+  FromParameterSource
   deriving (Show)
 
--- Used for creating quotient manifolds, the map is required to be a boolean map.
-data EquivalenceRelationship = EquivalenceRelationship Manifold Manifold Map
+-- The domain must be the CartesianProduct of n discrete TopologicalSpaces, with a total cardinality equal to the number of parameters, 
+-- and n equal to the dimensionality of the parameter source.
+data ParameterSource = ParameterSource [Double] TopologicalSpace
+  
+-- Used for creating quotient TopologicalSpaces, the map is required to be a boolean map.
+data EquivalenceRelationship = EquivalenceRelationship TopologicalSpace TopologicalSpace Map
   deriving (Show)
   
-data Manifold = 
+data TopologicalSpace = 
   Reals |
-  BooleanManifold |
+  BooleanTopologicalSpace |
   Labels SetOfLabels |
-  Product [Manifold] |
-  DisjointUnion SetOfLabels (Label->Manifold) |
+  Product [TopologicalSpace] |
+  DisjointUnion SetOfLabels (Label->TopologicalSpace) |
+  -- The Map must be a BooleanMap, the resulting TopologicalSpace is the subset where the BooleanMap evaluates to True.
   SimpleSubset Map |
-  Quotient Manifold Manifold EquivalenceRelationship
+  Quotient TopologicalSpace TopologicalSpace EquivalenceRelationship
   deriving (Show)
-
 
 -- Tests
 real2 = Product [Reals, Reals]
@@ -73,7 +85,7 @@ real3 = Product [Reals, Reals, Reals]
 
 elementIds = labelsFromIntegerRange 1 4
   
-f :: Label->Manifold
+f :: Label->TopologicalSpace
 f "1" = Reals
 f _ = Reals
 
@@ -83,4 +95,13 @@ m2 = Product [real2, Labels elementIds]
 
 map1 = Project { factor=1, domain=m2, codomain=real2}
 
+x = RealVariable "x"
+x' = RealVariableE x
+
+
+expression1 :: BooleanExpression
+expression1 =  (x' `LessThan` (RealConstant 1))  `And` ( (RealConstant 0) `LessThan` x')
+
+map2 = BooleanMap { domain=Reals, predicate=expression1 }
+unitLineSegment = SimpleSubset map2
 -- Just playing with Haskell Syntax here for convenience.  Will eventually delete everything below this line, and this comment.
