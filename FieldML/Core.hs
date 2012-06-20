2d35b6fddef6d85870c2d1a220b064925d1e9ff1
Randall Britten <r.britten@auckland.ac.nz>
2012-06-20 13:25:23 +1200

    Exploring the idea of adding a separate data type for equation.  About to revert, will stick to just using Equal Map for now.

--- FieldML/Core.hs
+++ FieldML/Core.hs
@@ -207,8 +207,13 @@ data DomainMap =
   DomainMapIf SetOfLabels DomainMap DomainMap
   
   deriving (Show, Eq)
-  
 
+
+-- | Equation f g Asserts that f == g.  
+
+-- Todo: clarify how this relates to the boolean Map Equal f g, which may evaluate to True or False.
+-- Could just have a collection somewhere of equal maps that are asserted to be true.
+data Equation = Equation Map Map
 
 
 -- Focus here is on *processing* the FieldML data structures.  
--- FieldML_test1.hs
+++ FieldML_test1.hs
@@ -189,3 +189,12 @@ pressureAtNodes =
 
 prop_testResult_IntParam_01d = ( validateMap pressureAtNodes )
 
+-- Demonstrating equations: And raises the question as to whether to just have Equals.  Note: ModML essentially has both Equals and Equation.
+xy1 = GeneralVariable "xy" (CartesianProduct [Reals, Reals])
+g1 = Tuple [ GeneralVariable "x" Reals, RealConstant 1.93 ]
+
+-- - This one has free variables xy and x, and whether it is true depends on values for xy and x. If there were a way of asserting that it must be true, then that constrains what valid values of xy and x are.
+equation1Style1 = xy1 `Equal` g1
+
+-- - This one just 
+equation1Style2 = Equation xy1 g1
