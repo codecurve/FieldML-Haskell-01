a2c4d958429d1293371bbf178b0609b3fab6b766
Randall Britten <r.britten@auckland.ac.nz>
2013-11-01 18:10:40 +1300

    Remove reference to unused test source code files.

--- test/FieldML-Haskell-test.hs
+++ test/FieldML-Haskell-test.hs
@@ -4,11 +4,7 @@ module Main where
 
 import Test.Framework
 
-import {-@ HTF_TESTS @-} FieldML.Utility01Test
-import {-@ HTF_TESTS @-} FieldML.Library02Test
-import {-@ HTF_TESTS @-} FieldML.Library01Test
 import {-@ HTF_TESTS @-} FieldML.CoreTest
-import {-@ HTF_TESTS @-} FieldML_test_mesh01Test
 
 main :: IO()
 main = htfMain htf_importedTests
